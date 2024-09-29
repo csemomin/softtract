57dd959c26815bf2a96491d470cd1b3787b3c9bc
Momin Ali <momin.ali@apsissolutions.com>
2024-09-29 12:41:03 +0600

    DB CHANGE

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php  
 try {
-    // $db = new PDO('mysql:host=localhost;dbname=softtrac_softtract;charset=utf8', 'softtrac_softtract', 'fk3VQ,YWiMm)');
-    $db = new PDO('mysql:host=localhost;dbname=softtrac_softtract;charset=utf8', 'root', '');
+    $db = new PDO('mysql:host=localhost;dbname=softtrac_softtract;charset=utf8', 'softtrac_softtract', 'fk3VQ,YWiMm)');
+    // $db = new PDO('mysql:host=localhost;dbname=softtrac_softtract;charset=utf8', 'root', '');
 } catch (PDOException $ex) {
     die('Database connection error!');
 }
