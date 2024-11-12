b41b83e781eca7da4b3affce99b93076da697a59
Momin Ali <momin.ali@apsissolutions.com>
2024-11-12 15:05:26 +0600

    php code remove

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php  
-try {
-    $db = new PDO('mysql:host=localhost;dbname=softtrac_softtract;charset=utf8', 'softtrac_softtract', 'fk3VQ,YWiMm)');
-    // $db = new PDO('mysql:host=localhost;dbname=softtrac_softtract;charset=utf8', 'root', '');
-} catch (PDOException $ex) {
-    die('Database connection error!');
-}
-    
